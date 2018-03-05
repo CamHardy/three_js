ef94c42c0803a79ae82a9fd071633c18563f15c3
Cameron Hardy <camfhardy@gmail.com>
2018-03-04 23:47:26 -0700

    now the ball is lit by colored lights, and the camera orbits around. Refactored functions addSphere/Light to createSphere/Light

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ window.onload = function () {
 	const ASPECT = WIDTH / HEIGHT;
 	const NEAR = 0.1;
 	const FAR = 10000;
+	const CAMERA_R = 300;
+
+	var time = 0;
 
 	// get the DOM element to attach to
 	const container = document.getElementById('the_zone');
@@ -25,6 +28,7 @@ window.onload = function () {
 	const scene = new THREE.Scene();
 
 	// add the camera to the scene
+	camera.position.z = CAMERA_R;
 	scene.add(camera);
 
 	// start the renderer
@@ -33,29 +37,43 @@ window.onload = function () {
 	// attach the renderer-supplied DOM element
 	container.appendChild(renderer.domElement);
 
-	addSphere(scene);
-	addLight(scene);
+	scene.add(createSphere());
+	scene.add(createLight(0, 0,  150, 0xFF0000));
+	scene.add(createLight(0, 0, -150, 0xFF0000));
+	scene.add(createLight(0,  150, 0, 0x00FF00));
+	scene.add(createLight(0, -150, 0, 0x00FF00));
+	scene.add(createLight( 150, 0, 0, 0x0000FF));
+	scene.add(createLight(-150, 0, 0, 0x0000FF));
 
 	function update() {
-	// draw the scene!
-	renderer.render(scene, camera);
+		// draw the scene!
+		renderer.render(scene, camera);
 
-	// schedule the next frame
-	requestAnimationFrame(update);
+		// advance time
+		time += 0.02 % (2 * Math.PI);
+
+		// move camera
+		camera.position.x = CAMERA_R * Math.cos(time);
+		camera.position.y = CAMERA_R * Math.sin(time) / 2;
+		camera.position.z = CAMERA_R * Math.sin(time);
+		camera.lookAt(scene.position);
+
+		// schedule the next frame
+		requestAnimationFrame(update);
 	}
 
 	// schedule the first frame
 	requestAnimationFrame(update);
 }
 
-function addSphere(scene) {
+function createSphere() {
 	// set up the sphere constants
 	const RADIUS = 50;
 	const SEGMENTS = 16;
 	const RINGS = 16;
 	const sphereMaterial = new THREE.MeshLambertMaterial(
 	{
-		color: 0x0000CC
+		color: 0xFFFFFF
 	});
 
 	// create a new mesh with sphere geometry
@@ -66,22 +84,17 @@ function addSphere(scene) {
 			RINGS),
 		sphereMaterial);
 
-	// move the sphere where we can see it
-	sphere.position.z = -300;
-
-	// add the sphere to the scene
-	scene.add(sphere);
+	return sphere;
 }
 
-function addLight(scene) {
+function createLight(x, y, z, color) {
 	// create a point light
-	const pointLight = new THREE.PointLight(0xFFFFFF);
+	const pointLight = new THREE.PointLight(color);
 
 	// set the light's position
-	pointLight.position.x = 10;
-	pointLight.position.y = 50;
-	pointLight.position.z = 130;
+	pointLight.position.x = x;
+	pointLight.position.y = y;
+	pointLight.position.z = z;
 
-	// add to the scene
-	scene.add(pointLight);
+	return pointLight;
 }
