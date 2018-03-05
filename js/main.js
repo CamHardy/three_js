b7512f39099b9fe8e00ed22500528726b788e9da
Cameron Hardy <camfhardy@gmail.com>
2018-03-04 17:24:35 -0700

    first commit!

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// main.js
+
+// do nothing (for now).
+window.onload = function () {
+	var text = document.createTextNode('This script is doing nothing (for now).');
+	document.getElementById('the_zone').appendChild(text);
+}
